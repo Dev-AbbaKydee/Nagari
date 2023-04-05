2dcacde23eb1d1cf5a639b7a41f55d4ecfeedf3c
A Moses <129866548+Dev-AbbaKydee@users.noreply.github.com>
2023-04-05 06:21:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function changename(params){
+    const a = document.getElementById("js")
+    const inputvalue = document.getElementById("inputfield").value;
+    a.innerHTML = inputvalue;
+    if(inputvalue !="Kydee") {
+        alert("Its not Kydee");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+z
